62784e5612f2bc7bdbb911ce3e820b0d3d068ccf
ravilella2018 <ravilella.2018@gmail.com>
2019-02-19 13:09:00 +0530

    Log4j Updated

--- NeeProject/src/com/durgasoft/launching/chromebrowser/BasePage.java
+++ NeeProject/src/com/durgasoft/launching/chromebrowser/BasePage.java
@@ -3,19 +3,29 @@ package com.durgasoft.launching.chromebrowser;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.spi.Configurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class BasePage 
 {
 	
 	public static WebDriver driver;
 	public static String path="./testdata.properties";
+	public static String logPath="./log4j.properties";
 	
+	static
+	{
+		PropertyConfigurator.configure(logPath);
+		System.out.println("iam default static block");
+	}
 	
 	public static String loadData(String key) throws Exception
 	{
@@ -56,12 +66,37 @@ public class BasePage
 	
 	public static void type(String keyLocator, String dataKey) throws Exception 
 	{
-		driver.findElement(By.id(loadData(keyLocator))).sendKeys(loadData(dataKey));
+		//driver.findElement(By.id(loadData(keyLocator))).sendKeys(loadData(dataKey));
+		getElement(keyLocator).sendKeys(loadData(dataKey));
 	}
 	
+	public static WebElement getElement(String keyLocator) throws Exception 
+	{
+		WebElement  element=null;
+		
+		if(keyLocator.endsWith("_id"))
+			element=driver.findElement(By.id(loadData(keyLocator)));
+		else if(keyLocator.endsWith("_name"))
+			element=driver.findElement(By.name(loadData(keyLocator)));
+		else if(keyLocator.endsWith("_xpath"))
+			element=driver.findElement(By.xpath(loadData(keyLocator)));
+		else if(keyLocator.endsWith("_link"))
+			element=driver.findElement(By.linkText(loadData(keyLocator)));
+		return element;
+		
+	}
+
 	public static void click(String keyLocator) throws Exception 
 	{
-		driver.findElement(By.xpath(loadData(keyLocator))).click();
+		getElement(keyLocator).click();
+	}
+	
+	public static void selectValue(String keyLocator, int keyItem) throws Exception
+	{
+		//getElement(keyLocator).sendKeys(loadData(keyItem));
+		WebElement ele = getElement(keyLocator);
+		Select s=new Select(ele);
+		s.selectByIndex(keyItem);
 	}
 	
 
